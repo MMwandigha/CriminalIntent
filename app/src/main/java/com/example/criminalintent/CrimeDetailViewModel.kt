ebfb6c5d86849147f900f7424305b9bef45cd431
ADMIN <maxmeandigha@gmail.com>
2023-07-05 23:51:40 +0300

    Complete

--- app/src/main/java/com/example/criminalintent/CrimeDetailViewModel.kt
+++ app/src/main/java/com/example/criminalintent/CrimeDetailViewModel.kt
@@ -2,8 +2,9 @@ package com.example.criminalintent
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.Transformations
+//import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.switchMap
 import java.io.File
 import java.util.UUID
 
@@ -12,10 +13,13 @@ class CrimeDetailViewModel(): ViewModel () {
     private val crimeRepository = CrimeRepository.get()
     private val crimeIdLiveData = MutableLiveData<UUID>()
 
+//    var crimeLiveData: LiveData<Crime?> =
+//        Transformations.switchMap(crimeIdLiveData) {
+//            crimeId -> crimeRepository.getCrime(crimeId)
+//        }
+
     var crimeLiveData: LiveData<Crime?> =
-        Transformations.switchMap(crimeIdLiveData) {
-            crimeId -> crimeRepository.getCrime(crimeId)
-        }
+        crimeIdLiveData.switchMap { crimeId -> crimeRepository.getCrime(crimeId) }
 
     fun loadCrime(crimeId: UUID) {
         crimeIdLiveData.value = crimeId
